5213fc60ac93464190e6d8a3d3a458325b8adb06
slchris <chris@lesscrowds.org>
2020-08-05 13:58:47 +0800

    add readme

---
+++ init.el
@@ -0,0 +1,35 @@
+;; Init.el file
+
+;; disbale init message
+(setq inhibit-startup-message t)
+;; emacs package manager
+;; http://melpa.org/
+;; use-package
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+
+;; init-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(use-package try
+  :ensure t)
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(which-key try use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
