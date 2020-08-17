b63c97c17b500d312ef7dd4cb0371bb0c1336002
slchris <chris@lesscrowds.org>
2020-08-17 21:15:01 +0800

    update

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 
 ;; disbale init message
 (setq inhibit-startup-message t)
+;; tool bar mod
+(tool-bar-mode -1)
 ;; emacs package manager
 ;; http://melpa.org/
 ;; use-package
@@ -21,12 +23,79 @@
 (use-package which-key
   :ensure t
   :config (which-key-mode))
+
+
+;; org-mode
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (require 'org-bullets)
+  (add-hook 'org-mode-hook #'org-bullets-mode))
+
+
+
+(setq indo-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(defalias 'list-buffers 'ibuffer)
+;;(defalias 'list-buffers 'ibuffer-other-windows)
+
+
+;(use-package tabbar
+;  :ensure t
+;  :config
+;  (tabbar-mode 1))
+
+;; swiper
+
+(use-package counsel
+  :ensure t
+  )
+(use-package swiper
+  :ensure t
+  :config
+  (progn
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t)
+    ;; enable this if you want `swiper' to use it
+    ;; (setq search-default-mode #'char-fold-to-regexp)
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "C-c C-r") 'ivy-resume)
+    (global-set-key (kbd "<f6>") 'ivy-resume)
+    (global-set-key (kbd "M-x") 'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+    (global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+    (global-set-key (kbd "<f1> l") 'counsel-find-library)
+    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+    (global-set-key (kbd "C-c g") 'counsel-git)
+    (global-set-key (kbd "C-c j") 'counsel-git-grep)
+    (global-set-key (kbd "C-c k") 'counsel-ag)
+    (global-set-key (kbd "C-x l") 'counsel-locate)
+    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+    (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+    ))
+;; ACE Windows
+
+
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)))
+
+;; auto stuff
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key try use-package)))
+ '(package-selected-packages
+   '(org-gtd ace-window arc-window tabbar tabber counsel swiper org-bullets which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
