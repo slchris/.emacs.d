30300a8d6913e7dddf1128de9b088c2d9d254c5f
slchris <chris@lesscrowds.org>
2020-09-09 09:23:08 +0800

    update

--- init.el
+++ init.el
@@ -1,2 +1,14 @@
 ;; Init.el file
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ace-window counsel org-bullets which-key try use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
